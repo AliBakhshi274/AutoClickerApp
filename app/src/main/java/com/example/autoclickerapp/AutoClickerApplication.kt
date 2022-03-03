980c61da4d550ac510db7e4ddfea88e373e6a161
astronaut <alibakhshi.bs@gmail.com>
2022-03-03 20:48:59 +0330

    navigation Component added

---
+++ app/src/main/java/com/example/autoclickerapp/AutoClickerApplication.kt
@@ -0,0 +1,12 @@
+package com.example.autoclickerapp
+
+import android.app.Application
+
+class AutoClickerApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+
+}
